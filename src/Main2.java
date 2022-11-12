0369629777b10ba1f9fc24e4db331e02761c60fb
bowBoy19 <daniluvat@gmail.com>
2022-11-13 02:40:19 +0500

    RACalc
    racalc

---
+++ src/Main2.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+
+    public class Main2 {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine();
+        String arr[] = str.split(" ");
+        int numArr[] = new int[arr.length];
+        sout
+
+    }
+
